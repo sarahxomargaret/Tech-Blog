270509ebc97f84e8ce197b7d1c359069fac2ce9f
Fawn <sarahmargaret@ymail.com>
2023-08-22 19:23:28 -0400

    added connection to db and server

--- server.js
+++ server.js
@@ -8,3 +8,34 @@ const app = express();
 const PORT = process.env.PORT || 5020;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'secret',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    }),
+  };
+  
+  app.use(session(sess));
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(routes);
+  
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
